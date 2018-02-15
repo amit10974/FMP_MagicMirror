42df32933655671742c7a217717a00b41c67b61d
RedayY <reday@hotmail.de>
2018-02-15 21:42:13 +0000

    Initial Commit
    Added Facial Landmark Detector
    Added Face Detector
    Added average example images
    added note file for citation and use
    added example screenshots

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # FMP_MagicMirror
 Repository for Magic Mirror 
+
+Middlesex University Final Major Computing Science Project.
+Code and Contents by (C) Reday Ahmad Yahya.
+
+Core Function:
+
+- Capture Face
+- Calculate Facial landmarks
+- Compare against Attractive DB Data
+- Use data in real time to output information to Magic Mirror Overlay
+
+________________________________________________________________________
+
+If time allows the Magic Mirror Overlay will aim to provide
+
+Potential Side Functions:
+
+- Time and Date
+- News Snippets and Headlines (Global and local)
+- Weather Forecast for the day
+- Snapshot Mode (Including viewing of previous taken pictures)
+- Gesture Control
+- Voice Assistant Control (Featuring Google Now or Amazon Alexa)
+- Clothing Suggestion based on weather feedback
+- Calendar expansion, displays future events on the mirror (e.g Dentist Appointment)
+- Horoscopes
+
+___________________________________________________________________________
+
+Libraries Included:
+
+OPEN CV by Intel
+DLIB
+MagicMirrorFramework
+raspberry pi camera module
+
+______________________________________________________________________________
+
+Research based various Journal Papers:
+
+pyimageresearch
+Project Proposal
+Description
+Literature Review
+Hardware Specification Documentation
+
+all by Reday Yahya, will be available soon.
---
+++ facial_landmarks.py
@@ -0,0 +1,55 @@
+#Code by Reday Yahya
+#02/02/2018
+#Facial Landmark detection using OpenCV, DLIB with added help of imageutilities by pyimageresearch
+
+#Packages needed (IMUTILS by pyimageresearch to install = pip install imutils)
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+
+#Cunstructing the argument parser for CL
+ap = argparse.ArgumentParser()
+
+#DLIB's trained facial Landmark detector
+ap.add_argument("-p", "--shape-predictor", required = True,
+    help = "path to Facial landmark predictor")
+
+#Path to Image we want to use to analyse (can use live feed too)
+ap.add_argument("-i", "--image", required = True,
+    help = "path to input image")
+
+args = vars(ap.parse_args())
+
+#DLIB face detectore with facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+#Detect face with openCV
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width = 500)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+rects = detector(gray, 1)
+
+#Getting the right region for the face
+for (i, rect) in enumerate(rects):
+	shape = predictor(gray, rect)
+	shape = face_utils.shape_to_np(shape)
+	
+        #Converting DLIB's rectange to OpenCV style
+	(x, y, w, h) = face_utils.rect_to_bb(rect)
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        #Mutli Face Count and Output //-Might not need remove possibly
+	cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        #Looping over the coordinates for the facial landmarks and display them on image
+	for (x, y) in shape:
+		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+
+#Output
+cv2.imshow("Output", image)
+cv2.waitKey(0)
