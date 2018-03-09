d0e3a967dc2d601fd56cda5d8433d978c6dc9f3e
RedayY <reday@hotmail.de>
2018-03-09 23:35:20 +0000

    Small hotfix for Statistical Analysis
    - Fixed Structure added further Images for analysis
    - Removed unused code
    - Cleaned Structure of Folder

--- training.py
+++ training.py
@@ -1,22 +1,32 @@
+#Code by Reday Yahya
+#09/03/2018
+#Facial Landmark detection using OpenCV, DLIB with added help of imageutilities by pyimageresearch
+#Document for Statistical Analysis and Data Gathering
+#Using Standard Deviation and Variance
+
+#Packages
 from combo import getFaceCord
 import os
 import statistics
 
-
+#Source Location
 source_img = os.listdir("/home/pi/Desktop/facial-landmarks/Material/attr/")
 source_path = "/home/pi/Desktop/facial-landmarks/Material/attr/"
 ctr = "0"
 
-#2D array with shapes from all trainig images
+#Collective Array of Coordinates from all Images distrubuted
 train_array = []
+
+#Loops through Images in Analysis Material Folder for attractive Females and passes it trhough getFaceCord()
 for x in source_img:
     print(x)
     train_array.append(getFaceCord(source_path + x, "output_results/output " + ctr +".txt"))
 
-#Sorting the Coordinates in an array
+#Taking gathered Coordinates and sorting them for Statistical Analysis Calculation
 x_arr = []
 y_arr = []
 sub_length = len(train_array[0])
+
 for i in range(sub_length):
     tmp_x = []
     tmp_y = []
@@ -26,14 +36,14 @@ for i in range(sub_length):
     x_arr.append(tmp_x)
     y_arr.append(tmp_y)
     
-print("X Array")
-print(x_arr)
+#Devug Output of Sorted Coordinate array. Sorted for the facial Landmarks Markup 68 locations
+# print("X Array")
+# print(x_arr)
+# print("Y Array")
+# print(y_arr)
 
-print("Y Array")
-print(y_arr)
 
-
-#THIS IS YOUR LEARNT MODEL
+#Collection of Learned Data
 average_mean_x = []
 average_mean_y = []
 average_sd_x = []
@@ -41,17 +51,19 @@ average_sd_y = []
 average_vari_x = []
 average_vari_y = []
 
+#Looping through Values for Calculations
 for x in x_arr:
 	average_sd_x.append(statistics.stdev(x))
 	average_mean_x.append(statistics.mean(x))
 	average_vari_x.append(statistics.variance(x))
 
+#Looping through Values for Calculations
 for x in y_arr:
 	average_sd_y.append(statistics.stdev(x))
 	average_mean_y.append(statistics.mean(x))
 	average_vari_y.append(statistics.variance(x))
 
-
+#Output
 print("STDEV for X coordinates")
 print(average_sd_x)
 print(" ")
@@ -60,7 +72,6 @@ print(average_mean_x)
 print(" ")
 print("variance for X coordinates")
 print(average_vari_x)
-
 print(" ")
 print("STDEV for Y coordinates")
 print(average_sd_y)
@@ -70,7 +81,3 @@ print(average_mean_y)
 print(" ")
 print("variance for Y coordinates")
 print(average_vari_y)
-
-    
-    
-    
